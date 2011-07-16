c6b3995fe468995abdfefdaf6f7ad501adcc7f52
Brian Leathem <bleathem@gmail.com>
2011-07-16 12:36:23 -0700

    cleanup of the hello component names

--- src/main/java/ca/bleathem/richfaces/hello/component/AbstractHello.java
+++ src/main/java/ca/bleathem/richfaces/hello/component/AbstractHello.java
@@ -6,7 +6,7 @@ import org.richfaces.cdk.annotations.*;
  * A trivial hello world component
  */
 
-@JsfComponent(type = "ca.bleathem.hello", family = "ca.bleathem.text", renderer = @JsfRenderer(type = "ca.bleathem.hello"), tag = @Tag(name="hello"))
+@JsfComponent(type = "ca.bleathem.richfaces.hello.Hello", family = "ca.bleathem.text", renderer = @JsfRenderer(type = "ca.bleathem.hello"), tag = @Tag(name="hello"))
 abstract public class AbstractHello extends javax.faces.component.UIComponentBase {
 
 }
