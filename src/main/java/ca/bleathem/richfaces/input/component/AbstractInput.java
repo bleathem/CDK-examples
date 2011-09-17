dd2321f8ced3c29cc6dcbc47f1a1db881b378995
Brian Leathem <bleathem@gmail.com>
2011-09-17 10:27:37 -0700

    Cleanup for the "Input" blog post

--- src/main/java/ca/bleathem/richfaces/input/component/AbstractInput.java
+++ src/main/java/ca/bleathem/richfaces/input/component/AbstractInput.java
@@ -1,14 +1,19 @@
 package ca.bleathem.richfaces.input.component;
 
+import org.richfaces.cdk.annotations.Attribute;
 import org.richfaces.cdk.annotations.JsfComponent;
 import org.richfaces.cdk.annotations.JsfRenderer;
 import org.richfaces.cdk.annotations.Tag;
 
 /**
- * A trivial hello world component
+ * A trivial input component
  */
 
-@JsfComponent(type = "ca.bleathem.richfaces.input.Input", family = "ca.bleathem.input", renderer = @JsfRenderer(type = "ca.bleathem.input"), tag = @Tag(name="input"))
-abstract public class AbstractInput extends javax.faces.component.UIComponentBase {
+@JsfComponent(
+        type = "ca.bleathem.richfaces.input.Input",
+        family = "ca.bleathem.input",
+        renderer = @JsfRenderer(type = "ca.bleathem.input"),
+        tag = @Tag(name="input"))
+abstract public class AbstractInput extends javax.faces.component.UIInput {
 
 }
